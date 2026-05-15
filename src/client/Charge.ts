35770f59e13e1e65b2a8f4147d1dd8e45557ec2b
Emre Dincoglu <Dincoglue@gmail.com>
2026-05-15 11:03:47 -0400

    Addressed comments

--- src/client/Charge.ts
+++ src/client/Charge.ts
@@ -19,7 +19,7 @@ export function charge(parameters: charge.Parameters) {
     async createCredential({ challenge }) {
       const { request, expires } = challenge
       const { account } = parameters
-      
+
       const amount = BigInt(request.amount);
       const currency = request.currency as Address;
       const recipient = request.recipient as Address;
